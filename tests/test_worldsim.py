a6f342996a514f3ca6c50605f89613df3d840c2f
Michael Noel Jones <mnjones+git@gmail.com>
2016-05-31 22:59:59 +0100

    More tests passing!

--- tests/test_worldsim.py
+++ tests/test_worldsim.py
@@ -1,6 +1,6 @@
 # test_worldsim.py
 from unittest import TestCase
-from worldsim import World, Player
+from worldsim import World, Player, LinkCommand, MoveCommand
 
 
 class TestWorldsim(TestCase):
@@ -29,6 +29,80 @@ class TestWorldsim(TestCase):
             (51.258472, -1.076191)
         )
 
+    def create_portals(self):
+        portals = {}
+        locations = [
+            (51.258472, -1.076191),
+            (51.258623, -1.081081),
+            (51.260184, -1.088666),
+            (51.260287, -1.083540),
+            (51.259231, -1.084654),
+            (51.258658, -1.081617),
+            (51.261578, -1.083193),
+            (51.262180, -1.082827),
+            (51.261766, -1.082850),
+            (51.261324, -1.083513),
+            (51.261720, -1.082287),
+            (51.259443, -1.083362),
+            (51.260750, -1.083664),
+            (51.260892, -1.081706),
+            (51.259620, -1.081563),
+            (51.260552, -1.081721),
+            (51.260315, -1.081542),
+        ]
+
+        names = [
+            ("Southern Entrance To War Memorial"),
+            ("If Aught of Oaten"),
+            ("The Bounty Inn Pub"),
+            ("I Saw The Hare"),
+            ("Diana Stanley Memorial Plaque"),
+            ("War Memorial Playground"),
+            ("Torch"),
+            ("Civic Centre War Memorial"),
+            ("War Memorial Park"),
+            ("Memorial Park Aviary"),
+            ("Shelter Dedication Plaque"),
+            ("The Golden Tree Engraving"),
+            ("Memorial Band Stand"),
+            ("Kath And Gordon Ball Memorial"),
+            ("Winnie White Memorial Bench"),
+            ("Tree Planting Ceremony Plaque"),
+            ("Ivy & George White Plaque"),
+        ]
+        guids = [
+            "47db8ce5d774463f9a8e7aef948e8093.16",
+            "7d2c6ea6b74d40d18147ee73dbcd802f.16",
+            "19bee196d8964d378807c8e7effce827.16",
+            "2d9e4c7fe8dc48679e3a0f59b04287aa.16",
+            "4f00ca7279b14c16b1107939be671ea7.16",
+            "2ad0c389e094410384f10f74fd2e1250.16",
+            "b629547a87d7432d90f8e27577e7dc99.16",
+            "9c7e56e4e505450faeb0f81389cc80bf.11",
+            "4b1b025b6fbe460e980d4db347993ca9.16",
+            "c7c1ca91df9145b0bc3b894033d1ea4f.11",
+            "41de7df2cc3a4e91b5a2f9c24e92e4e2.16",
+            "bd059ba73a7c4307a8b04920ff7806dc.16",
+            "032428ea97494692a221337cf19287b2.16",
+            "dc9b9457816c47f69766f3e2c29e8c4b.16",
+            "f3a3d5806b2446caa3df67432700a49e.16",
+            "2286f18bad3e47fea698d38cd5f01f39.16",
+            "fb88168cc2774b1cac1da52daa7bd40a.16",
+        ]
+        portals = [
+            self.world.add_portal(
+                name=x[0],
+                location=x[1],
+                guid=x[2]
+            )
+            for x in zip(names, locations, guids)
+        ]
+        return portals
+
+    create_two_portals = create_portals
+    create_three_portals = create_portals
+    create_four_portals = create_portals
+
     def test_link_portals(self):
         """
             Verify Link Creation.
@@ -36,23 +110,10 @@ class TestWorldsim(TestCase):
             After creating a link, it should be possible to
             verify from either portal
         """
-        portals = {}
-        portals["1"] = self.world.add_portal(
-            name="Southern Entrance To War Memorial",
-            guid="47db8ce5d774463f9a8e7aef948e8093.16",
-            location=(51.258472,  -1.076191)
-        )
-        # ID "If Aught of Oaten" AS 2
-        # LOCATE 2 AT 51.258623,  -1.081081
-        # GUID 2 7d2c6ea6b74d40d18147ee73dbcd802f.16
-        portals["2"] = self.world.add_portal(
-            name="If Aught of Oaten",
-            guid="7d2c6ea6b74d40d18147ee73dbcd802f.16",
-            location=(51.258623,  -1.081081)
-        )
-        self.world.create_link(portals["1"], portals["2"])
-        self.assertTrue(self.world.link_exists(portals["1"], portals["2"]))
-        self.assertTrue(self.world.link_exists(portals["2"], portals["1"]))
+        portals = self.create_two_portals()
+        self.world.create_link(portals[0], portals[1])
+        self.assertTrue(self.world.link_exists(portals[0], portals[1]))
+        self.assertTrue(self.world.link_exists(portals[1], portals[0]))
 
     def test_add_player_to_the_world(self):
         """
@@ -64,3 +125,159 @@ class TestWorldsim(TestCase):
         self.world.add_player(player)
         self.assertEqual(player.world, self.world)
         self.assertEqual(player, self.world.players[0])
+
+    def add_player_to_world(self):
+        player = Player()
+        self.world.add_player(player)
+        return player
+
+    def test_player_command_link(self):
+        """
+            A player may have a command to link two portals.
+            The existence of the command on the player should not cause
+            the portals to be linked in the world until it is executed.
+        """
+        player = self.add_player_to_world()
+        portals = self.create_two_portals()
+        player.location = portals[0].location
+        command = LinkCommand(portal1=portals[0], portal2=portals[1])
+        player.add_command(command)
+        self.assertFalse(self.world.link_exists(portals[0], portals[1]))
+        player.command[0]()
+        self.assertTrue(self.world.link_exists(portals[0], portals[1]))
+
+    def test_player_cannot_link_from_afar(self):
+        """
+            A player cannot execute a command to link a portal
+            if they are not located within 40m of that portal
+        """
+        player = self.add_player_to_world()
+        portals = [
+            p for y in [
+                "Southern Entrance To War Memorial",
+                "The Bounty Inn Pub",
+                "Winnie White Memorial Bench",
+                "I Saw The Hare"
+            ] for p in self.create_four_portals() if p.name == y
+        ]
+        player.add_command(MoveCommand(portals[3].location))
+        player.command[0]()
+        command = LinkCommand(portal1=portals[0], portal2=portals[1])
+        player.add_command(command)
+        self.assertFalse(self.world.link_exists(portals[0], portals[1]))
+        player.command[1]()
+        self.assertFalse(self.world.link_exists(portals[0], portals[1]))
+
+    def test_player_links_three_portals_into_field(self):
+        """
+            A player connects three portals with links.
+            As well as the links created, a field is formed in the `world`.
+        """
+        player = self.add_player_to_world()
+        portals = [
+            p for y in [
+                "Southern Entrance To War Memorial",
+                "The Bounty Inn Pub",
+                "Winnie White Memorial Bench",
+            ] for p in self.create_four_portals() if p.name == y
+        ]
+        player.add_command(MoveCommand(portals[0].location))
+        player.add_command(LinkCommand(
+            portal1=portals[0],
+            portal2=portals[1]
+        ))
+        player.add_command(LinkCommand(
+            portal1=portals[0],
+            portal2=portals[2]
+        ))
+        player.add_command(MoveCommand(portals[1].location))
+        player.add_command(LinkCommand(
+            portal1=portals[1],
+            portal2=portals[2]
+        ))
+        self.assertFalse(self.world.link_exists(portals[0], portals[1]))
+        self.assertFalse(self.world.link_exists(portals[0], portals[2]))
+        self.assertFalse(self.world.link_exists(portals[1], portals[2]))
+        self.assertFalse(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[2]
+        ))
+        for command in player.commands:
+            command()
+        self.assertTrue(self.world.link_exists(portals[0], portals[1]))
+        self.assertTrue(self.world.link_exists(portals[0], portals[2]))
+        self.assertTrue(self.world.link_exists(portals[1], portals[2]))
+        self.assertTrue(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[2]
+        ))
+
+    def test_larger_field_is_chosen(self):
+        """
+            A player connects three portals with links.
+            As well as the links created, a field is formed in the `world`.
+        """
+        player = self.add_player_to_world()
+        portals = [
+            p for y in [
+                "Southern Entrance To War Memorial",
+                "The Bounty Inn Pub",
+                "Winnie White Memorial Bench",
+                "Ivy & George White Plaque"
+            ] for p in self.create_four_portals() if p.name == y
+        ]
+        player.add_command(MoveCommand(portals[2].location))
+        player.add_command(LinkCommand(
+            portal1=portals[2],
+            portal2=portals[0]
+        ))
+        player.add_command(LinkCommand(
+            portal1=portals[2],
+            portal2=portals[1]
+        ))
+        player.add_command(MoveCommand(portals[3].location))
+        player.add_command(LinkCommand(
+            portal1=portals[3],
+            portal2=portals[0]
+        ))
+        player.add_command(LinkCommand(
+            portal1=portals[3],
+            portal2=portals[1]
+        ))
+        player.add_command(MoveCommand(portals[0].location))
+        player.add_command(LinkCommand(
+            portal1=portals[0],
+            portal2=portals[1]
+        ))
+        self.assertFalse(self.world.link_exists(portals[2], portals[0]))
+        self.assertFalse(self.world.link_exists(portals[2], portals[1]))
+        self.assertFalse(self.world.link_exists(portals[3], portals[0]))
+        self.assertFalse(self.world.link_exists(portals[3], portals[1]))
+        self.assertFalse(self.world.link_exists(portals[0], portals[1]))
+        self.assertFalse(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[2]
+        ))
+        self.assertFalse(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[3]
+        ))
+        for command in player.commands:
+            command()
+        self.assertTrue(self.world.link_exists(portals[0], portals[1]))
+        self.assertTrue(self.world.link_exists(portals[0], portals[2]))
+        self.assertTrue(self.world.link_exists(portals[1], portals[2]))
+        self.assertFalse(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[2]
+        ))
+        self.assertTrue(self.world.field_exists(
+            portals[0],
+            portals[1],
+            portals[3]
+        ))
--- worldsim.py
+++ worldsim.py
@@ -1,15 +1,91 @@
+import functools
+
+
+def area_of_triangle(point1, point2, point3):
+    (x1, y1, x2, y2, x3, y3) = (point1 + point2 + point3)
+    return abs((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3))
+
+
+class Command(object):
+    def __init__(self, world=None, player=None):
+        self._world = world
+        self._player = player
+
+    def __call__(self):
+        pass
+
+    @property
+    def world(self):
+        return self._world
+
+    @property
+    def player(self):
+        return self._player
+
+    @world.setter
+    def world(self, val):
+        self._world = val
+
+    @player.setter
+    def player(self, val):
+        self._player = val
+
+
+class LinkCommand(Command):
+    def __init__(self, portal1=None, portal2=None, **kwargs):
+        super(LinkCommand, self).__init__(**kwargs)
+        self._portal1 = portal1
+        self._portal2 = portal2
+
+    def __call__(self):
+        # print "Creating Link from {} to {}".format(
+            # self._portal1.name,
+            # self._portal2.name
+        # )
+        if self.player.location == self._portal1.location:
+            self._world.create_link(self._portal1, self._portal2)
+        else:
+            print "Link failed - player not within range of {} ({})".format(
+                self._portal1.name,
+                self._portal1.location
+            )
+
+
+class MoveCommand(Command):
+    def __init__(self, location=None, **kwargs):
+        super(MoveCommand, self).__init__(**kwargs)
+        self._location = location
+
+    def __repr__(self):
+        return "MoveCommand(location={}, world={}, player={})".format(
+            self._location,
+            self._world,
+            self._player
+        )
+
+    def __call__(self):
+        # print "Setting player location to {}".format(self._location)
+        self.player.location = self._location
+
+
 class Player(object):
     def __init__(self, world=None, commands=None):
         self._world = world
-        self._command_list = commands
+        self._command_list = commands or []
         self.location = None
 
     @property
     def commands(self):
         return self._command_list
 
+    @property
+    def command(self):
+        return self._command_list
+
     def add_command(self, command):
-        self.command.append(command)
+        self.commands.append(command)
+        command.world = self.world
+        command.player = self
 
     def create_link(self, portal_from, portal_to):
         world.create_link(portal_from, portal_to)
@@ -23,14 +99,19 @@ class Portal(object):
         self.name = name
         self.guid = guid
         self.location = location
-        self.linked_portals = []
+        self.outbound_links = []
+        self.inbound_links = []
 
     def is_linked_to(self, portal):
-        return portal in self.linked_portals
+        return portal in self.outbound_links
+
+    def is_linked_from(self, portal):
+        return portal in self.inbound_links
 
     def add_link(self, portal):
-        if portal not in self.linked_portals:
-            self.linked_portals.append(portal)
+        if (not self.is_linked_to(portal) and not self.is_linked_from(portal)):
+            self.outbound_links.append(portal)
+            portal.inbound_links.append(self)
 
 
 class Link(object):
@@ -42,6 +123,10 @@ class Link(object):
 class Field(object):
     def __init__(self, portals=[]):
         self._portals = portals
+
+    @property
+    def portals(self):
+        return self._portals
 
 
 class World(object):
@@ -63,6 +148,10 @@ class World(object):
     def player(self):
         return self._players
 
+    @property
+    def fields(self):
+        return self._fields
+
     def add_portal(self, name=None, guid=None, location=None):
         portal = Portal(
             name=name,
@@ -71,6 +160,13 @@ class World(object):
         )
         self._portals.append(portal)
         return portal
+
+    def field_exists(self, portal1, portal2, portal3):
+        expected_portals = set([portal1, portal2, portal3])
+        for field in self.fields:
+            if set(field.portals) == expected_portals:
+                return True
+        return False
 
     def link_exists(self, portal_one, portal_two):
         assert portal_one in self._portals, (
@@ -84,6 +180,16 @@ class World(object):
             portal_two.is_linked_to(portal_one)
         )
 
+    def area_of_field(self, portal1, portal2, portal3):
+        return area_of_triangle(
+            portal1.location,
+            portal2.location,
+            portal3.location
+        )
+
+    def create_field(self, portal1, portal2, portal3):
+        self.fields.append(Field([portal1, portal2, portal3]))
+
     def create_link(self, portal_one, portal_two):
         assert portal_one in self._portals, (
             "Unknown portal, {}".format(portal_one)
@@ -91,7 +197,33 @@ class World(object):
         assert portal_two in self._portals, (
             "Unknown portal, {}".format(portal_two)
         )
+        print "\nLinking {} to {}".format(portal_one.name, portal_two.name)
         portal_one.add_link(portal_two)
+        print "outbound_links for {}: {}".format(
+            portal_one.name,
+            ",".join(p.name for p in portal_one.outbound_links)
+        )
+        print "outbound_links for {}: {}".format(
+            portal_two.name,
+            ",".join(p.name for p in portal_two.outbound_links)
+        )
+        # Are there any common linked portals for portal_one and portal_two
+        potential_field_portals = (
+            (set(portal_one.outbound_links) | set(portal_one.inbound_links)) &
+            (set(portal_two.outbound_links) | set(portal_two.inbound_links))
+        )
+        print "potential fields created with: {}\n\n".format(
+            potential_field_portals
+        )
+        if potential_field_portals:
+            size_key = functools.partial(
+                self.area_of_field,
+                portal_one,
+                portal_two
+            )
+            max_field_portal = max(potential_field_portals, key=size_key)
+            print "max size field created to: {}".format(max_field_portal.name)
+            self.create_field(portal_one, portal_two, max_field_portal)
 
     def add_player(self, player):
         self.players.append(player)
