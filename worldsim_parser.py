703fe873b80396aceef7123f43c45e3fb2323d6c
Michael Noel Jones <mnjones+git@gmail.com>
2016-06-06 23:15:34 +0100

    Added remaining parsers

--- worldsim_parser.py
+++ worldsim_parser.py
@@ -100,4 +100,63 @@ def WorldsimParser(ParserElement):
     field_ref = (
         pid | field_request
     )
+# <capture-ref> ::= <id> | <capture-request>
+    capture_ref = Forward()
+    capture_ref <<= (
+        pid | capture_request
+    )
+# <destroy-link-ref> ::= <id> | <destroy-link-request>
+    destroy_link_request = Forward()
+    destroy_link_request <<= (
+        pid | destroy_link_request
+    )
+# <destroy-portal-ref> ::= <id> | <destroy-portal-request>
+    destroy_portal_ref = Forward()
+    destroy_portal_ref = (
+        pid | destro_portal_ref
+    )
+# <deploy-ref> ::= <id> | <deploy-request>
+    deploy_ref = (pid | deploy_request)
+# <link-sequence> ::= <link-ref> [<link-sequence>]
+    link_sequence = Forward()
+    link_sequence <<= link_ref + Optional(link_sequence)
+# <portal> ::= <portalname> | <portalguid> | <latlng>
+    portal = portalname | portalguid | latlng
+# <latlng> ::= <lat>[,] <lng>
+    latlng = lat + Optional(Literal(',')) + lng
 
+# <intorfloat> ::= <digit>+ ['.' <digit>+]
+# <lat> ::= ['-'] <intorfloat | <intorfloat>  ['N' | 'S']
+    lat = real + Or(Literal('N'), Literal('S'))
+# <lng> ::= ['-'] <intorfloat> | <intorfloat> ['E' | 'W']
+    lng = real + Or(Literal('E'), Literal('W'))
+# <portalguid> ::= <id>
+# <id> ::= <hexdigit>+
+
+# <command-entry> ::= <field-ref> |
+#                     <capture-ref> |
+#                     <destroy-link-ref>
+#                     <destroy-portal-ref> |
+#                     <deploy-ref> |
+#                     <link-sequence>
+    command_entry = (
+        field_ref |
+        capture_ref |
+        destroy_link_ref |
+        destroy_portal_ref |
+        deploy_ref |
+        link_sequence
+    )
+# <command-sequence> ::= SEQ <command-entry> [<command-sequence>]
+    command_sequence = (
+        Ignore(Keyword("SEQ")) +
+        command_entry +
+        Optional(command_sequence)
+    )
+# <locate-command> ::= LOCATE <portal> [AT] <latlng>
+    locate_command = (
+        Ignore(Keyword('LOCATE')) +
+        portal +
+        Ignore(Optional(Keyword('AT'))) +
+        latlng
+    )
