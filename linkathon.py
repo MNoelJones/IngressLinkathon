4e1378cdfaa35593d24adac3dd4fb61a82a6cd32
Michael Noel Jones <mnjones+git@gmail.com>
2016-07-23 22:54:58 +0100

    Add PEP8ification

--- linkathon.py
+++ linkathon.py
@@ -1,19 +1,22 @@
 def bary(p1, p2, p3, p):
+    """
+        Calculate barycentre of point p wrt triangle with vertices p1, p2, p3.
+    """
     (x1, y1) = p1
     (x2, y2) = p2
     (x3, y3) = p3
     (x, y) = p
     try:
         a = (
-                (y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)
-            ) / (
-                (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
-            )
+            (y2 - y3) * (x - x3) + (x3 - x2) * (y - y3)
+        ) / (
+            (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
+        )
         b = (
-                (y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)
-            ) / (
-                (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
-            )
+            (y3 - y1) * (x - x3) + (x1 - x3) * (y - y3)
+        ) / (
+            (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3)
+        )
     except BaseException as err:
         raise ValueError(err)
     c = 1 - a - b
@@ -21,14 +24,20 @@ def bary(p1, p2, p3, p):
 
 
 def point_in_triangle(p, p1, p2, p3):
+    """
+        Determine if the point p is inside the triangle with
+        vertices p1, p2, p3.
+    """
     try:
         (a, b, c) = bary(p1, p2, p3, p)
     except:
         print("Exception {} {} {} {}".format(p1, p2, p3, p))
         return False
-    return (0 <= a <= 1 and
-            0 <= b <= 1 and
-            0 <= c <= 1)
+    return (
+        0 <= a <= 1 and
+        0 <= b <= 1 and
+        0 <= c <= 1
+    )
 
 
 class Portal(object):
